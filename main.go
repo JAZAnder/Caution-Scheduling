c119ea64104e9548a173ee74bf64562809c4a26a
Joshua Cantu <joshua@thecantu.com>
2023-09-06 07:35:14 +0000

    added auth routes

--- main.go
+++ main.go
@@ -2,16 +2,38 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"github.com/gorilla/mux"
-	"log"
-	"github.com/joho/godotenv"
-	"os"
 	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth"
+	"github.com/gorilla/sessions"
+	"github.com/markbates/goth/providers/google"
 )
 
 //Entry Point
 func main()  {
+
+	//Creating Google Login
+	key := "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"  // Replace with your SESSION_SECRET or similar
+	maxAge := 86400 * 30  // 30 days
+	isProd := false       // Set to true when serving over https
+	store := sessions.NewCookieStore([]byte(key))
+  	store.MaxAge(maxAge)
+  	store.Options.Path = "/"
+  	store.Options.HttpOnly = true   // HttpOnly should always be enabled
+  	store.Options.Secure = isProd
+
+	goth.UseProviders(
+		google.New("239635319851-g0dhkjdo7t00run1j0fb3rld3115434h.apps.googleusercontent.com", "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", "https://8080-cs-71058812811-default.cs-us-east1-rtep.cloudshell.dev/auth/google/callback", "email", "profile"),
+	)
+
+	//Ending Google Login
+
 
 	err := godotenv.Load()
 	if err != nil {
@@ -37,6 +59,10 @@ func newRouter() *mux.Router {
 	fs := http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/")))
 	r.PathPrefix("/assets/").Handler(fs)
 
+	//Google Signin
+	r.HandleFunc("/auth/{provider}", auth)
+	r.HandleFunc("/auth/{provider}/callback", authCallback)
+
 	return r
 }
 
@@ -48,4 +74,17 @@ func helloHandler(w http.ResponseWriter, r *http.Request){
 func indexHandler(w http.ResponseWriter, r *http.Request){
 	tmpl := template.Must(template.ParseFiles("./index.html"))
 	tmpl.Execute(w, nil)
+
+}
+func auth(w http.ResponseWriter, r *http.Request)  {
+	gothic.BeginAuthHandler(w, r)
+}
+func authCallback(w http.ResponseWriter, r *http.Request)  {
+	user, err := gothic.CompleteUserAuth(w, r)
+    if err != nil {
+      fmt.Fprintln(w, err)
+      return
+    }
+    t, _ := template.ParseFiles("templates/success.html")
+    t.Execute(w, user)
 }
