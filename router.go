63a13134d628ac7bd0ad87bc9a7e5c99e7ac8d43
Joshua Cantu <joshua@thecantu.com>
2023-09-15 00:24:20 -0500

    Started adding enpoints

--- router.go
+++ router.go
@@ -8,8 +8,9 @@ func newRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", helloHandler).Methods("GET")
 	r.HandleFunc("/", indexHandler)
+	r.HandleFunc("/testsql", helloHandler)
 
-
+// - - - - - - Static Reasourses - - - - - - 
 	//Assets
 	fs := http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/")))
 	r.PathPrefix("/assets/").Handler(fs)
@@ -18,7 +19,27 @@ func newRouter() *mux.Router {
 	wp := http.StripPrefix("/pages/", http.FileServer(http.Dir("./pages/")))
 	r.PathPrefix("/pages/").Handler(wp)
 
-	//Google Signin
+
+	
+// - - - - - - API Endpoints - - - - - - 
+
+//Labs
+	//Get List of All Labs
+	r.HandleFunc("/api/labs",helloHandler).Methods("GET")
+	//Adds a new lab based on Info in POST (Name) and Cookie (Session_Token)
+	r.HandleFunc("/api/labs/add", helloHandler).Methods("POST")
+
+//LabHours
+	//Gets a labs hours based on LabId
+	r.HandleFunc("/api/labHours/{labId}", helloHandler).Methods("GET")
+	//Changes lab hours based on LabId POST(action, hours) and Cookie(Session_Token)
+	r.HandleFunc("/api/labHours/add/{labId}", helloHandler).Methods("POST")
+//
+
+
+
+
+//Google Shit
 	r.HandleFunc("/auth/{provider}", auth)
 	r.HandleFunc("/auth/{provider}/callback", authCallback)
 
