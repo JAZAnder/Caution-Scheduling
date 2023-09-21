828e9d740da8e111930baf46af586a4e649a4d9d
Joshua Cantu <joshua@thecantu.com>
2023-09-20 20:58:26 -0500

    Restored Routes

--- routes.go
+++ routes.go
@@ -35,35 +35,15 @@ func (a *App) labRoutes(){
     a.Router.HandleFunc("/api/lab/{id:[0-9]+}", a.deleteLab).Methods("DELETE")
 }
 
-func newRouter() *mux.Router {
-	r := mux.NewRouter()
-	r.HandleFunc("/hello", helloHandler).Methods("GET")
-	r.HandleFunc("/", indexHandler)
-	r.HandleFunc("/testsql", helloHandler)
-
-	// - - - - - - Static Reasourses - - - - - -
+func (a *App) staticRoutes(){
 	//Assets
 	fs := http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/")))
-	r.PathPrefix("/assets/").Handler(fs)
+	a.Router.PathPrefix("/assets/").Handler(fs)
 
 	//Pages
 	wp := http.StripPrefix("/pages/", http.FileServer(http.Dir("./pages/")))
-	r.PathPrefix("/pages/").Handler(wp)
+	a.Router.PathPrefix("/pages/").Handler(wp)
 
-	r.HandleFunc("/", indexHandler)
-
-	// // - - - - - - API Endpoints - - - - - -
-
-	// //Labs
-	// 	//Get List of All Labs
-	// 	r.HandleFunc("/api/labs",helloHandler).Methods("GET")
-	// 	//Adds a new lab based on Info in POST (Name) and Cookie (Session_Token)
-	// 	r.HandleFunc("/api/labs/add", helloHandler).Methods("POST")
-
-	// //LabHours
-	// 	//Gets a labs hours based on LabId
-	// 	r.HandleFunc("/api/labHours/{labId}", helloHandler).Methods("GET")
-	// 	//Changes lab hours based on LabId POST(action, hours) and Cookie(Session_Token)
-	// 	r.HandleFunc("/api/labHours/add/{labId}", helloHandler).Methods("POST")
-	return r
+	a.Router.HandleFunc("/hello", helloHandler).Methods("GET")
+	a.Router.HandleFunc("/", indexHandler)
 }
