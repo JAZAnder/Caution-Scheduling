2e0fc5d8283cd357734060be9b0f39559bf52c23
Joshua Cantu <joshua@thecantu.com>
2023-09-26 07:52:55 -0500

    made username PK and started sign up methond

--- object-localuser.go
+++ object-localuser.go
@@ -1,14 +1,31 @@
 package main
-import()
+
+import (
+	"database/sql"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type localUser struct{
-	WNumber int `json:"wNumber"`
 	UserName string `json:"userName"`
 	FirstName string `json:"firstName"`
 	LastName string `json:"lastName"`
 	Email string `json:"email"`
-	EncryptedPassword string `json:"password"`
-	PasswordSalt string `json:"salt"`
+	Password string `json:"password"`
 	IsAdmin bool `json:"isAdmin"`
-	
+}
+
+func (u *localUser) login(db *sql.DB){
+
+}
+
+func (u *localUser) signUp(db *sql.DB) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), 8)
+	if err != nil {
+		return err; 
+	}
+	query := ""+string(hashedPassword)
+	_, err = db.Query(query) 
+
+	return err
 }
