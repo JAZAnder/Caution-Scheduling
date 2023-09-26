9d473d103d2a18c8a83ee12f51c4e800501b8099
Joshua Cantu <joshua@thecantu.com>
2023-09-26 07:53:42 -0500

    started checkcookie

--- object-sessioncookie.go
+++ object-sessioncookie.go
@@ -1,9 +1,18 @@
 package main
-import()
 
-type sessionCookie struct{
-	Id int `json:"id"`
-	WNumber int `json:"wnumber"`
-	Cookie string `json:"cookie"`
-	user localUser
+import "database/sql"
+
+type sessionCookie struct {
+	Id      int    `json:"id"`
+	WNumber int    `json:"wnumber"`
+	Cookie  string `json:"cookie"`
+	user    localUser
+}
+
+// func (c *sessionCookie) createSession (db *sql.DB) error {
+//  return err
+// }
+
+func (c *sessionCookie) checkSession(db *sql.DB, cookie string) bool {
+	return true
 }
