2da1578a462d2368db4a30a85068f21828c84142
Joshua Cantu <joshua@thecantu.com>
2023-10-06 02:18:38 -0500

    Working Login

--- src/pages/luser/login/login.js
+++ src/pages/luser/login/login.js
@@ -1,5 +1,16 @@
-function login(){
+async function login(){
     username = document.getElementById("username").value
     password = document.getElementById("password").value
-    userlogin(username,password)
+    data = await user_login(username,password)
+    if(data['error']){
+        document.getElementById("password").value = ""
+        document.getElementById("error").innerHTML = data['error']
+        console.log("Login Fail")
+    }else{
+        document.getElementById("username").value = ""
+        document.getElementById("password").value = ""
+        console.log("Logged In")
+        window.location.assign("../")
+    }
+    
 }
