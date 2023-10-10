8af9a7fae8c64b63981b519579b745cf5e2bac39
Joshua Cantu <joshua@thecantu.com>
2023-10-09 21:38:26 -0500

    fixed caps

--- src/object-hour.go
+++ src/object-hour.go
@@ -12,17 +12,17 @@ type hour struct{
 
 func (h *hour) getHour(db *sql.DB) error{
 	query := "SELECT startTime, endTime FROM hours WHERE id=" + strconv.Itoa(h.Id)
-	return db.QueryRow(query).Scan(&h.startTime, &h.endTime)
+	return db.QueryRow(query).Scan(&h.StartTime, &h.EndTime)
 }
 
 func (h *hour) updateHour(db *sql.DB) error{
-	query := "UPDATE `hours` SET `startTime` = '"+h.startTime+"', `endTime` = '"+endTime+"' WHERE `hours`.`id` = "+strconv.Itoa(h.Id)+""
+	query := "UPDATE `hours` SET `startTime` = '"+h.StartTime+"', `endTime` = '"+h.EndTime+"' WHERE `hours`.`id` = "+strconv.Itoa(h.Id)+""
 	_, err := db.Exec(query)
 	return err
 }
 
 func (h *hour) createHour(db *sql.DB) error{
-	query := "INSERT INTO `hours` (`startTime`, `endTime`) VALUES ('"+h.startTime+"','"+h.endTime+"')"
+	query := "INSERT INTO `hours` (`startTime`, `endTime`) VALUES ('"+h.StartTime+"','"+h.EndTime+"')"
 	err := db.QueryRow(query)
 
 	if err != nil{
@@ -44,7 +44,7 @@ func getHours(db *sql.DB) ([]hour, error){
 
 	for rows.Next(){
 		var h hour
-		if err:= rows.Scan(&h.Id, &h.startTime, &h.endTime); err!= nil{
+		if err:= rows.Scan(&h.Id, &h.StartTime, &h.EndTime); err!= nil{
 			return nil,err
 		}
 		hours = append(hours, h)
