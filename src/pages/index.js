2f3978dc76125bd1d32b1313bb8bedfdfd28a4ae
Joshua Cantu <joshua@thecantu.com>
2023-10-12 12:19:22 -0500

    Add lab function

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,28 @@ function importElements(elementId, url) {
     }
 }
 
+async function addLab(){
+    var options = document.getElementById("tutorId-Select")
+    var option = document.createElement("option")
+    data = await lab_getall()
+    if(data['error']){
+        document.getElementById("error").innerHTML = data['error']
+        console.log("error : " + data['error'])
+    }
+    data.forEach(addLabForEach)
+
+}
+function addLabForEach(item){
+    const optionInfo = new lab(item)
+    var options = document.getElementById("tutorId-Select")
+    var option = document.createElement("option")
+
+    option.text = optionInfo.name
+    option.value = optionInfo.Id
+
+    options.add(option)
+}
+
 // Load the readOnlyTable.html into the 'readOnlyTable' div
 importElements('readOnlyTable', './assets/elements/readOnlyTable.html');
 
