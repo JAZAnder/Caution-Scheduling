eee83bb3d7f350256dae26f4cc88ff6b6b2c694e
Joshua Cantu <joshua@thecantu.com>
2023-10-31 12:15:23 -0500

    Added createLabTimeSlot

--- src/object-labhour.go
+++ src/object-labhour.go
@@ -16,10 +16,28 @@ func (lh *labHour) getLabTimeslot (db *sql.DB) error{
 	var tempLabId string
 	var tempHourId string
 	var tempUserHourId string
-	query := "SELECT "
+	query := "SELECT Id, LabId, HoursId, TutorId FROM `labHours` WHERE `Id` = "+ strconv.Itoa(lh.Id) +";	"
 	err := db.QueryRow(query).Scan(&tempLabId, &tempHourId, &tempUserHourId)
 	lh.LabId, err = strconv.Atoi(tempLabId)
 	lh.HourId, err = strconv.Atoi(tempHourId)
 	lh.UserHourId, err = strconv.Atoi(tempUserHourId)
 	return err
 }
+
+func (lh *labHour) createLabTimeSlot (db *sql.DB) error{
+	query := "INSERT INTO `labHours` (`LabId`, `HoursId`, `TutorId`) VALUES ('"+strconv.Itoa(lh.LabId)+"', '"+strconv.Itoa(lh.HourId)+"', '"+strconv.Itoa(lh.UserHourId)+"');"
+	err := db.QueryRow(query)
+	if err != nil{
+		return err.Err()
+	}
+	return nil
+}
+
+// func (lh *labHour) deleteLabTimeSlot (db *sql.DB) error{
+
+// }
+
+// func (lh *labHour) changeTutor (db *sql.DB) error{
+
+// }
+
