0611f1f93ed94822c83b940e414712a090ac9a45
Joshua Cantu <joshua@thecantu.com>
2023-11-08 23:00:07 -0600

    Create Lab

--- src/assets/objects/lab.js
+++ src/assets/objects/lab.js
@@ -1,9 +1,10 @@
 const lab_getall_Url = baseUrl+"/api/labs"
+const lab_create_Url = baseUrl+"/api/lab"
 
 function lab(lab){
     this.Id = lab['id']
     this.name = lab['name']
-    this.location = ['location']
+    this.labLocation = lab['location']
 }
 
 async function lab_getall(){
@@ -14,6 +15,24 @@ async function lab_getall(){
       }; 
 
       const result = await fetch(lab_getall_Url, requestOptions)
+      const data = await result.json();
+      console.log(data)
+      return data
+}
+
+async function lab_create(name, lablocation){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("name", name);
+    urlencoded.append("location", lablocation);
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+    };
+    const result = await fetch(lab_create_Url, requestOptions)
       const data = await result.json();
       console.log(data)
       return data
