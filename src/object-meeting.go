b9398563456d109c01a9777ded88a044ebe967df
Joshua Cantu <joshua@thecantu.com>
2023-11-10 00:26:23 -0600

    Removed what looks like controller parts

--- src/object-meeting.go
+++ src/object-meeting.go
@@ -1,7 +1,8 @@
 package main
 
-import(
+import (
 	"database/sql"
+	"errors"
 	"strconv"
 )
 
@@ -17,8 +18,8 @@ func (m *meeting) getMeeting(db *sql.DB) error{
 	var tempTutorHourId string
 	var tempLabId string
 	query := "SELECT userHourId, labId, studentName, studentEmail FROM meetings WHERE id=" + strconv.Itoa(m.Id)
-	err := db.QueryRow(query).Scan(&tempUserHourId, &tempLabId, &m.StudentName, &m.StudentEmail)
-	m.TutorHourId, err = strconv.Atoi(tempUserHourId)
+	err := db.QueryRow(query).Scan(&tempTutorHourId, &tempLabId, &m.StudentName, &m.StudentEmail)
+	m.UserHourId, err = strconv.Atoi(tempTutorHourId)
 	m.LabId, err = strconv.Atoi(tempLabId)
 	return err
 }
@@ -33,29 +34,29 @@ func (m *meeting) updateMeeting(db *sql.DB) error{
 
 func (m *meeting) createMeeting(db *sql.DB) error{
 	var available bool
-	query = "SELECT `Available` FROM `UserHours` WHERE `userHourId` = " + m.userHourId
-	err = db.QueryRow(query).Scan(&available)
+	query := "SELECT `Available` FROM `UserHours` WHERE `userHourId` = " + strconv.Itoa(m.UserHourId)
+	err := db.QueryRow(query).Scan(&available)
 
 	if err != nil{
-		return err.Err()
+		return err
 	}
 	
 	if !available{
 		return errors.New("This tutor is not available for that given time")
 	}
 
-	query := "INSERT INTO `meetings` (`userHourId`, `labId`, `studentName`, `studentEmail`) VALUES ('"+strconv.Itoa(m.UserHourId)+"', '"+strconv.Itoa(m.LabId)+"', '"+m.StudentName+"', '"+m.StudentEmail+"');"
-	err := db.QueryRow(query)
+	query = "INSERT INTO `meetings` (`userHourId`, `labId`, `studentName`, `studentEmail`) VALUES ('"+strconv.Itoa(m.UserHourId)+"', '"+strconv.Itoa(m.LabId)+"', '"+m.StudentName+"', '"+m.StudentEmail+"');"
+	errsql := db.QueryRow(query)
 
-	if err != nil{
-		return err.Err()
+	if errsql.Err() != nil{
+		return errsql.Err()
 	}
 
-	query := "UPDATE `UserHours` SET `Available` = 'false' WHERE `userHourId` = " + m.userHourId
-	err := db.QueryRow(query)
+	query = "UPDATE `UserHours` SET `Available` = 'false' WHERE `userHourId` = " + strconv.Itoa(m.UserHourId)
+	errsql = db.QueryRow(query)
 
-	if err != nil{
-		return err.Err()
+	if errsql.Err() != nil{
+		return errsql.Err()
 	}
 
 	return nil
@@ -82,51 +83,49 @@ func getMeetings(db *sql.DB) ([]meeting, error){
 	return meetings, nil
 }
 
-func getMyMeetings(db *sql.DB) ([]meeting, error){
-	var c sessionCookie
-    cookie, err := r.Cookie("key")
-    if err != nil {
-        if errors.Is(err, http.ErrNoCookie) {
-            respondWithError(w, http.StatusUnauthorized, "Cookie not Found")
-            return
-        } else {
-            respondWithError(w, http.StatusInternalServerError, err.Error())
-            return
-        }
-    }
+// func getMyMeetings(db *sql.DB) ([]meeting, error){
+// 	var c sessionCookie
+//     cookie, err := r.Cookie("key")
+//     if err != nil {
+//         if errors.Is(err, http.ErrNoCookie) {
+//             return
+//         } else {
+//             return
+//         }
+//     }
 
-    c.Cookie = cookie.Value
+//     c.Cookie = cookie.Value
 
-    currentUser, err := c.checkSession(a.DB)
-    if err != nil {
-        if err == sql.ErrNoRows {
-            respondWithError(w, http.StatusUnauthorized, "Session Expired")
-            return
-        } else {
-            respondWithError(w, http.StatusInternalServerError, err.Error())
-            return
-        }
-    } 
+//     currentUser, err := c.checkSession(a.DB)
+//     if err != nil {
+//         if err == sql.ErrNoRows {
+//             respondWithError(w, http.StatusUnauthorized, "Session Expired")
+//             return
+//         } else {
+//             respondWithError(w, http.StatusInternalServerError, err.Error())
+//             return
+//         }
+//     } 
 
-	rows, err := db.Query("SELECT id, userHourId, labId, studentName, studentEmail FROM meetings m join userHours u on m.userHourId = u.id where u.userId = " + currentUser)
+// 	rows, err := db.Query("SELECT id, userHourId, labId, studentName, studentEmail FROM meetings m join userHours u on m.userHourId = u.id where u.userId = " + currentUser)
 
-	if err != nil{
-		return nil, err
-	}
+// 	if err != nil{
+// 		return nil, err
+// 	}
 
-	defer rows.Close()
+// 	defer rows.Close()
 
-	meetings := []meeting{}
+// 	meetings := []meeting{}
 
-	for rows.Next(){
-		var m meeting
-		if err := rows.Scan(&m.Id, &m.UserHourId, &m.LabId, &m.StudentName, &m.StudentEmail); err != nil{
-			return nil, err
-		}
-		meetings = append(meetings, m)
-	}
-	return meetings, nil
-}
+// 	for rows.Next(){
+// 		var m meeting
+// 		if err := rows.Scan(&m.Id, &m.UserHourId, &m.LabId, &m.StudentName, &m.StudentEmail); err != nil{
+// 			return nil, err
+// 		}
+// 		meetings = append(meetings, m)
+// 	}
+// 	return meetings, nil
+// }
 
 func (m *meeting) deleteMeeting(db *sql.DB) error{
 	query := "DELETE FROM `meetings` WHERE `meetings`.`Id`="+strconv.Itoa(m.Id)+""
