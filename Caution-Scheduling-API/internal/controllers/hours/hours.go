314848e03ca91237d495eb6a9b09a903a83f0a5b
Joshua Cantu <joshua@thecantu.com>
2024-09-03 08:54:57 -0500

    Removed dot imports

--- Caution-Scheduling-API/internal/controllers/hours/hours.go
+++ Caution-Scheduling-API/internal/controllers/hours/hours.go
@@ -6,9 +6,9 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	. "github.com/JAZAnder/Caution-Scheduling/internal/objects/hour"
-	. "github.com/JAZAnder/Caution-Scheduling/internal/objects/user"
-	. "github.com/JAZAnder/Caution-Scheduling/internal/helpers"
+	hour "github.com/JAZAnder/Caution-Scheduling/internal/objects/hour"
+	user "github.com/JAZAnder/Caution-Scheduling/internal/objects/user"
+	helpers "github.com/JAZAnder/Caution-Scheduling/internal/helpers"
 	"github.com/gorilla/mux"
 )
 
@@ -26,41 +26,41 @@ func AddHourRoutes(a *mux.Router){
 }
 
 var(
-	database = GetDatabase()
+	database = helpers.GetDatabase()
 )
 
 func getHour(w http.ResponseWriter, r *http.Request){
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		RespondWithError(w, http.StatusBadRequest, "Invalid hour Id")
+		helpers.RespondWithError(w, http.StatusBadRequest, "Invalid hour Id")
 		return
 	}
-	h := Hour{Id: id}
+	h := hour.Hour{Id: id}
 	err = h.GetHour(database)
 	if err != nil {
 		switch err {
 		case sql.ErrNoRows:
-			RespondWithError(w, http.StatusNotFound, "Timeslot not Found")
+			helpers.RespondWithError(w, http.StatusNotFound, "Timeslot not Found")
 		default:
-			RespondWithError(w, http.StatusInternalServerError, err.Error())
+			helpers.RespondWithError(w, http.StatusInternalServerError, err.Error())
 		}
 		return
 	}
-	RespondWithJSON(w, http.StatusOK, h)
+	helpers.RespondWithJSON(w, http.StatusOK, h)
 }
 
 func createHour(w http.ResponseWriter, r *http.Request){
-	var c SessionCookie
+	var c user.SessionCookie
 
 	cookie, err := r.Cookie("key")
 	if err != nil {
 		if errors.Is(err, http.ErrNoCookie) {
-			RespondWithError(w, http.StatusUnauthorized, "Cookie not Found")
+			helpers.RespondWithError(w, http.StatusUnauthorized, "Cookie not Found")
 			
 			return
 		} else {
-			RespondWithError(w, http.StatusInternalServerError, err.Error())
+			helpers.RespondWithError(w, http.StatusInternalServerError, err.Error())
 			return
 		}
 	}
@@ -70,84 +70,84 @@ func createHour(w http.ResponseWriter, r *http.Request){
 	currentUser, err := c.CheckSession(database)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			RespondWithError(w, http.StatusUnauthorized, "Session Expired")
+			helpers.RespondWithError(w, http.StatusUnauthorized, "Session Expired")
 			return
 		} else {
-			RespondWithError(w, http.StatusInternalServerError, err.Error())
+			helpers.RespondWithError(w, http.StatusInternalServerError, err.Error())
 			return
 		}
 	}
 
 	if !currentUser.IsAdmin {
 		fmt.Println("	Fail : Time Not Created by " + currentUser.UserName +" : "+ "Not an Admin")
-		RespondWithError(w, http.StatusForbidden, "Not an Admin")
+		helpers.RespondWithError(w, http.StatusForbidden, "Not an Admin")
 		return
 	}
 
-	var h Hour
+	var h hour.Hour
 	h.StartTime = r.PostFormValue("startTime")
 	h.EndTime = r.PostFormValue("endTime")
 	h.DayOfWeek, err = strconv.Atoi(r.PostFormValue("dayOfWeek"))
 
 	if (err != nil || h.DayOfWeek > 6 || h.DayOfWeek < 0) {
-		RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		helpers.RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
 
-	if IllegalString(h.StartTime) ||IllegalString(h.EndTime){
+	if helpers.IllegalString(h.StartTime) ||helpers.IllegalString(h.EndTime){
 		fmt.Println("	Fail : Time Not Created by " + currentUser.UserName +" : "+ "Invalid request payload")
-		RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		helpers.RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
 
 	err = h.CreateHour(database)
 	if err != nil {
 		fmt.Println("	Fail : Time Not Created by " + currentUser.UserName +" : "+ err.Error())
-		RespondWithError(w, http.StatusInternalServerError, err.Error())
+		helpers.RespondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	fmt.Println("	Time Created by " + currentUser.UserName)
 
-	RespondWithJSON(w, http.StatusCreated, h)
+	helpers.RespondWithJSON(w, http.StatusCreated, h)
 }
 
 func getHours(w http.ResponseWriter, r *http.Request){
-	hours, err := GetHours(database)
+	hours, err := hour.GetHours(database)
 	if err != nil {
-		RespondWithError(w, http.StatusInternalServerError, err.Error())
+		helpers.RespondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	RespondWithJSON(w, http.StatusOK, hours)
+	helpers.RespondWithJSON(w, http.StatusOK, hours)
 }
 
 func getHoursByDay(w http.ResponseWriter, r *http.Request){
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		RespondWithError(w, http.StatusBadRequest, "Invalid hour Id")
+		helpers.RespondWithError(w, http.StatusBadRequest, "Invalid hour Id")
 		return
 	}
 
-	hours, err := GetHoursByDay(database, id)
+	hours, err := hour.GetHoursByDay(database, id)
 	if err != nil {
-		RespondWithError(w, http.StatusInternalServerError, err.Error())
+		helpers.RespondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	RespondWithJSON(w, http.StatusOK, hours)
+	helpers.RespondWithJSON(w, http.StatusOK, hours)
 }
 
 func deleteHour(w http.ResponseWriter, r *http.Request){
-	var c SessionCookie
+	var c user.SessionCookie
 
 	cookie, err := r.Cookie("key")
 	if err != nil {
 		if errors.Is(err, http.ErrNoCookie) {
-			RespondWithError(w, http.StatusUnauthorized, "Cookie not Found")
+			helpers.RespondWithError(w, http.StatusUnauthorized, "Cookie not Found")
 			
 			return
 		} else {
-			RespondWithError(w, http.StatusInternalServerError, err.Error())
+			helpers.RespondWithError(w, http.StatusInternalServerError, err.Error())
 			return
 		}
 	}
@@ -157,33 +157,33 @@ func deleteHour(w http.ResponseWriter, r *http.Request){
 	currentUser, err := c.CheckSession(database)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			RespondWithError(w, http.StatusUnauthorized, "Session Expired")
+			helpers.RespondWithError(w, http.StatusUnauthorized, "Session Expired")
 			return
 		} else {
-			RespondWithError(w, http.StatusInternalServerError, err.Error())
+			helpers.RespondWithError(w, http.StatusInternalServerError, err.Error())
 			return
 		}
 	}
 
 	if !currentUser.IsAdmin {
 		fmt.Println("	Fail : Time Not Deleted by " + currentUser.UserName +" : "+ "Not an Admin")
-		RespondWithError(w, http.StatusForbidden, "Not an Admin")
+		helpers.RespondWithError(w, http.StatusForbidden, "Not an Admin")
 		return
 	}
 
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		RespondWithError(w, http.StatusBadRequest, "Invalid hour ID")
+		helpers.RespondWithError(w, http.StatusBadRequest, "Invalid hour ID")
 		return
 	}
-	h := Hour{Id: id}
+	h := hour.Hour{Id: id}
 	if err := h.DeleteHour(database); err != nil {
-		RespondWithError(w, http.StatusInternalServerError, err.Error())
+		helpers.RespondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
-	RespondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
+	helpers.RespondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
 
 }
 
@@ -191,14 +191,14 @@ func getUsersByHour(w http.ResponseWriter, r *http.Request){
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		RespondWithError(w, http.StatusBadRequest, "Invalid hour Id")
+		helpers.RespondWithError(w, http.StatusBadRequest, "Invalid hour Id")
 		return
 	}
 
-	users, err := GetUsersByHour(database, id)
+	users, err := user.GetUsersByHour(database, id)
 	if err != nil {
-		RespondWithError(w, http.StatusInternalServerError, err.Error())
+		helpers.RespondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	RespondWithJSON(w, http.StatusOK, users)
+	helpers.RespondWithJSON(w, http.StatusOK, users)
 }
