b6da1bd1c2f01210fa1d23ab55db8f427c6377cb
Joshua Cantu <joshua@thecantu.com>
2024-09-02 23:20:14 -0500

    All routes are set

--- Caution-Scheduling-API/internal/app/routes.go
+++ Caution-Scheduling-API/internal/app/routes.go
@@ -3,58 +3,25 @@ package app
 import (
 	"net/http"
 	. "github.com/JAZAnder/Caution-Scheduling/internal/controllers/hours"
+	. "github.com/JAZAnder/Caution-Scheduling/internal/controllers/labs"
+	. "github.com/JAZAnder/Caution-Scheduling/internal/controllers/users"
+	. "github.com/JAZAnder/Caution-Scheduling/internal/controllers/meetings"
 
 )
 
 //Routes
 
 func (a *App) initializeRoutes() {
-    a.labRoutes()
-	a.userRoutes()
+	AddLabRoutes(a.Router)
+	AddUserRoutes(a.Router)
 	AddHourRoutes(a.Router)
-	a.meetingRoutes()
+	AddMeetingRoutes(a.Router)
 	a.staticRoutes()
 }
 
-func (a *App) labRoutes(){
-	a.Router.HandleFunc("/api/labs", a.getLabs).Methods("GET")
-    a.Router.HandleFunc("/api/lab", a.createLab).Methods("POST")
-    a.Router.HandleFunc("/api/lab/{id:[0-9]+}", a.getLab).Methods("GET")
-    a.Router.HandleFunc("/api/lab/{id:[0-9]+}", a.updateLab).Methods("PUT")
-    a.Router.HandleFunc("/api/lab/{id:[0-9]+}", a.deleteLab).Methods("DELETE")
-	a.Router.HandleFunc("/api/lab/timeslot/{id:[0-9]+}", a.openLabTimeSlot).Methods("POST")
-	a.Router.HandleFunc("/api/lab/timeslots", a.getAllLabHours).Methods("GET")
-	a.Router.HandleFunc("/api/lab/timeslot/{id:[0-9]+}", a.removeLabTimeSlot).Methods("DELETE")
-}
 
 
 
-func (a *App) userRoutes(){
-	a.Router.HandleFunc("/api/luser", a.createLocalUser).Methods("POST")
-	a.Router.HandleFunc("/api/luser/login", a.loginLocalUser).Methods("POST")
-	a.Router.HandleFunc("/api/luser/whoami", a.whoami).Methods("GET")
-	a.Router.HandleFunc("/api/luser/logout", a.logoutLocalUser).Methods("DELETE")
-	a.Router.HandleFunc("/api/lusers", a.getAllUsers).Methods("GET")
-	a.Router.HandleFunc("/api/luser/resetmypasswd",a.changePassword).Methods("PUT")
-	a.Router.HandleFunc("/api/luser/admin/resetpasswd", a.resetPassword).Methods("PUT")
-	a.Router.HandleFunc("/api/luser/timeslot", a.addTime).Methods("POST")
-	a.Router.HandleFunc("/api/luser/admin/timeslot", a.addTimeAdmin).Methods("POST")
-	a.Router.HandleFunc("/api/luser/admin/timeslot/{id:[0-9]+}", a.removeTimeAdmin).Methods("DELETE")
-	a.Router.HandleFunc("/api/tutor/availability/{username}", a.getluserAvalibleTime).Methods("GET")
-	a.Router.HandleFunc("/api/tutor/hours/{username}",a.getluserTime).Methods("GET")
-	a.Router.HandleFunc("/api/tutor/timeslot/whois/{id:[0-9]+}", a.getUserHourById).Methods("GET")
-	a.Router.HandleFunc("/api/tutor/timeslots",a.getAllUserHours).Methods("GET")
-	a.Router.HandleFunc("/api/tutor/whois/{id}", a.getUserInfo).Methods("GET")
-
-}
-
-func (a *App) meetingRoutes(){
-	a.Router.HandleFunc("/api/meeting", a.createMeeting).Methods("POST")
-	a.Router.HandleFunc("/api/meeting/{id:[0-9]+}",a.getMeeting).Methods("GET")
-	a.Router.HandleFunc("/api/meetings", a.getMeetings).Methods("GET")
-	a.Router.HandleFunc("/api/meeting/{id:[0-9]+}",a.deleteMeeting).Methods("DELETE")
-	a.Router.HandleFunc("/api/meetings/mine", a.getMyMeetings).Methods("GET")
-}
 
 func (a *App) staticRoutes(){
 	//Assets
@@ -66,6 +33,4 @@ func (a *App) staticRoutes(){
 	rf := http.StripPrefix("/", http.FileServer(http.Dir("./pages/")))
 	a.Router.PathPrefix("/").Handler(rf)
 
-
-    a.Router.HandleFunc("/hello", helloHandler).Methods("GET")
 }
