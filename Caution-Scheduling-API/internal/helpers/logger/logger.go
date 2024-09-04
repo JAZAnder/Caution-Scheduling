10dca19a87eb28590b838a31735b465ed4607cbe
Joshua Cantu <joshua@thecantu.com>
2024-09-04 11:10:36 -0500

    Created logger

---
+++ Caution-Scheduling-API/internal/helpers/logger/logger.go
@@ -0,0 +1,95 @@
+package logger
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+)
+
+// Level 1 Debug
+// Level 2 Info
+// Level 3 Warning
+// Level 4 Error
+// Level 5 Critical
+
+// Type
+
+var (
+	minLogLevelConsole int = 1
+	minLogLevelDb      int = 1
+)
+
+func LogSetUpDb(miLogLevel int) error {
+	if miLogLevel > 5 || miLogLevel < 1 {
+		err := errors.New("level is not within range")
+		return err
+	}
+
+	minLogLevelDb = miLogLevel
+	return nil
+}
+
+func LogSetUpCon(miLogLevel int) error {
+	if miLogLevel > 5 || miLogLevel < 1 {
+		err := errors.New("level is not within range")
+		return err
+	}
+
+	minLogLevelConsole = miLogLevel
+	return nil
+}
+
+func Log(level int, category string ,message string) error {
+	var err error
+	var levelStr string
+	if level > 5 || level < 1 {
+		err = errors.New("level is not within range")
+		return err
+	}
+
+	currentTime := time.Now()
+	time := currentTime.Format("2006-01-02 15:04:05")
+
+	if level == 1 {
+		levelStr = "Debug"
+	} else if level == 2 {
+		levelStr = "Info"
+	} else if level == 3 {
+		levelStr = "Warning"
+	} else if level == 4 {
+		levelStr = "Error"
+	} else if level == 5 {
+		levelStr = "Critical"
+	}
+
+	if level >= minLogLevelConsole {
+		err = printMessageToConsole(time, levelStr, category, message)
+		if err != nil {
+			return err
+		}
+	}
+
+	if level >= minLogLevelDb {
+		err = logMessageInDatabase(time, levelStr, category, message)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func printMessageToConsole(time string, level string, category string, message string) error {
+
+	fmt.Println("\n " + level + " " + time + " Category: " + category)
+	fmt.Println("\t" + message + "\n")
+
+	return nil
+}
+
+func logMessageInDatabase(time string, level string, category string, message string) error {
+
+	return nil
+}
