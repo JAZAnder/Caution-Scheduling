29cd9843a323a22cc42da9782ea50be8a7de5b2c
Joshua Cantu <joshua@thecantu.com>
2024-09-04 12:12:48 -0500

    Added logging to database

--- Caution-Scheduling-API/internal/helpers/logger/logger.go
+++ Caution-Scheduling-API/internal/helpers/logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"database/sql"
 	"errors"
 	"fmt"
 	"time"
@@ -12,21 +13,25 @@ import (
 // Level 3 Warning
 // Level 4 Error
 // Level 5 Critical
+// Level 6 NoLogging
 
 // Type
 
 var (
 	minLogLevelConsole int = 1
-	minLogLevelDb      int = 1
+	minLogLevelDb      int = 6
 )
 
-func LogSetUpDb(miLogLevel int) error {
+var database *sql.DB
+
+func LogSetUpDb(miLogLevel int, db *sql.DB) error {
 	if miLogLevel > 5 || miLogLevel < 1 {
 		err := errors.New("level is not within range")
 		return err
 	}
 
 	minLogLevelDb = miLogLevel
+	database = db
 	return nil
 }
 
@@ -40,11 +45,19 @@ func LogSetUpCon(miLogLevel int) error {
 	return nil
 }
 
-func Log(level int, category string, subCategory string, user string ,message string) error {
+func Log(level int, category string, subCategory string, user string, message string) error {
 	var err error
 	var levelStr string
 	if level > 5 || level < 1 {
 		err = errors.New("level is not within range")
+		return err
+	}
+	if len(category) > 20{
+		err = errors.New("category is too long")
+		return err
+	}
+	if len(subCategory) > 20{
+		err = errors.New("subCategory is too long")
 		return err
 	}
 
@@ -81,15 +94,20 @@ func Log(level int, category string, subCategory string, user string ,message st
 	return nil
 }
 
-func printMessageToConsole(time string, level string, category string, subCategory string, user string ,  message string) error {
+func printMessageToConsole(time string, level string, category string, subCategory string, user string, message string) error {
 
-	fmt.Println("\n " + level + " || " + time + " || Category: " + category +" - " + subCategory + " || "+ user)
+	fmt.Println("\n " + level + " || " + time + " || Category: " + category + " - " + subCategory + " || " + user)
 	fmt.Println("\t" + message + "\n")
 
 	return nil
 }
 
-func logMessageInDatabase(time string, level string, category string, subCategory string, user string,  message string) error {
+func logMessageInDatabase(time string, level string, category string, subCategory string, user string, message string) error {
 
+	query := "Insert Into logs (`level`, `category`, `subCategory`, `user`, `message`) VALUES (`"+level+"`,`"+category+"`,`"+subCategory+"`,`"+user+"`,`"+message+"`)"
+	_, err := database.Exec(query)
+	if err != nil {
+		return err
+	}
 	return nil
 }
