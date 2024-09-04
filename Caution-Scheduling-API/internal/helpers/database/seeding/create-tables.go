1faaf144158a837aa244fb97fed1c36ba0226fcd
Joshua Cantu <joshua@thecantu.com>
2024-09-04 11:32:19 -0500

    All tables created by Golang

--- Caution-Scheduling-API/internal/helpers/database/seeding/create-tables.go
+++ Caution-Scheduling-API/internal/helpers/database/seeding/create-tables.go
@@ -14,6 +14,10 @@ func CreateTables(db *sql.DB) {
 	createLocalUserTables()
 	createHoursTable()
 	createLabsTable()
+	createMeetingsTable()
+	createLabHoursTable()
+	createUserHoursTable()
+	createSessionCookieTable()
 }
 
 func createLocalUserTables() {
@@ -72,6 +76,86 @@ func createLabsTable() {
 		logger.Log(4, "database", err.Error())
 	} else {
 		logger.Log(2, "database", "labs table either created or already existed")
+	}
+
+}
+
+func createMeetingsTable() {
+	query := "CREATE TABLE IF NOT EXISTS meetings(" +
+		"`tutorHourId` int DEFAULT NULL," +
+		"`labId` int DEFAULT NULL," +
+		"`studentName` varchar(255) NOT NULL" +
+		"`studentEmail` varchar(255) NOT NULL," +
+		"`date` BIGINT NOT NULL );"
+
+	logger.Log(2, "database", "Creating meetings table")
+
+	logger.Log(1, "database", query)
+
+	_, err := database.Exec(query)
+	if err != nil {
+		logger.Log(4, "database", err.Error())
+	} else {
+		logger.Log(2, "database", "meetings table either created or already existed")
+	}
+
+}
+
+func createLabHoursTable() {
+	query := "CREATE TABLE `MYSQL_DATABASE`.`labHours` (" +
+		"`id` INT AUTO_INCREMENT PRIMARY KEY, " +
+		"`labId` int DEFAULT NULL," +
+		"`hourId` INT NOT NULL , " +
+		"`tutorId` INT NOT NULL) ;"
+
+	logger.Log(2, "database", "Creating labHours table")
+
+	logger.Log(1, "database", query)
+
+	_, err := database.Exec(query)
+	if err != nil {
+		logger.Log(4, "database", err.Error())
+	} else {
+		logger.Log(2, "database", "labHours table either created or already existed")
+	}
+
+}
+
+func createUserHoursTable() {
+	query := "CREATE TABLE IF NOT EXISTS userHours(" +
+		"`id` INT AUTO_INCREMENT PRIMARY KEY, " +
+		"`username` VARCHAR(255) NOT NULL," +
+		"`hoursId` INT NOT NULL , " +
+		"`available` tinyint(1) NOT NULL DEFAULT '1' ) ;"
+
+	logger.Log(2, "database", "Creating userHours table")
+
+	logger.Log(1, "database", query)
+
+	_, err := database.Exec(query)
+	if err != nil {
+		logger.Log(4, "database", err.Error())
+	} else {
+		logger.Log(2, "database", "userHours table either created or already existed")
+	}
+
+}
+
+func createSessionCookieTable() {
+	query := "CREATE TABLE IF NOT EXISTS `sessionCookie`(" +
+		"`id` INT AUTO_INCREMENT PRIMARY KEY, " +
+		"`cookie` VARCHAR(255) NOT NULL UNIQUE KEY," +
+		"`username` VARCHAR(255) NOT NULL );"
+
+	logger.Log(2, "database", "Creating sessionCookie table")
+
+	logger.Log(1, "database", query)
+
+	_, err := database.Exec(query)
+	if err != nil {
+		logger.Log(4, "database", err.Error())
+	} else {
+		logger.Log(2, "database", "sessionCookie table either created or already existed")
 	}
 
 }
