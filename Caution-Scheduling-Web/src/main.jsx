4998f05f84f047c63970b40aa2732c4182ffe665
Joshua Cantu <joshua@thecantu.com>
2024-09-09 09:03:49 -0500

    Added router

--- Caution-Scheduling-Web/src/main.jsx
+++ Caution-Scheduling-Web/src/main.jsx
@@ -1,11 +1,28 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Home from './Home.jsx'; 
+import ReactDOM from 'react-dom/client'
+import NavigationBar from './navigationBar.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Home /> 
-  </StrictMode>,
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <NavigationBar/>,
+    children:[
+      {
+        path: "",
+        element: <Home/>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <RouterProvider router={router} />
+)
