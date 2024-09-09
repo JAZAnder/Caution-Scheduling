161a6be7b423e8366d125b115252e011ed549bad
Joshua Cantu <joshua@thecantu.com>
2024-09-09 09:04:25 -0500

    Moved Nav bar to own file

--- Caution-Scheduling-Web/src/Home.jsx
+++ Caution-Scheduling-Web/src/Home.jsx
@@ -8,49 +8,6 @@ import "./App.css";
 function Home() {
   return (
     <>
-      <header className="topheader">
-        <a href="/" className="header-logo-link">
-          <img
-            src={schedulingLogo}
-            alt="Caution Scheduling Logo"
-            className="header-logo"
-          />
-        </a>
-        <span className="center-text">Caution Scheduling</span>
-        <nav>
-          <ul className="nav-list">
-            <li>
-              <a href to="/">Home</a>
-            </li>
-            <li>
-              <a href="#employee">Employee Login</a>
-            </li>
-            <li>
-              <a href="#otherlink">Other Link</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
-      <header className="bottomheader">
-        <nav>
-          <ul className="nav-list">
-            <li>
-              <a href="#schedule">Lab Schedule</a>
-            </li>
-            <li>
-              <a href="#meeting">Schedule a Meeting</a>
-            </li>
-            <li>
-              <a href="#intolab">Sign into Lab</a>
-            </li>
-            <li>
-              <a href="#virtual">Join Virtually</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
       <main id="root">
         <video autoPlay loop muted>
           <source src={labVideo} type="video/mp4" />
---
+++ Caution-Scheduling-Web/src/navigationBar.jsx
@@ -0,0 +1,53 @@
+import { Outlet } from "react-router-dom";
+import schedulingLogo from "./assets/CautionSchedulingLogoUpdate.png";
+import { Link } from 'react-router-dom';
+
+export default function NavigationBar() {
+    return (
+        <>
+            <header className="topheader">
+                <a href="/" className="header-logo-link">
+                    <img
+                        src={schedulingLogo}
+                        alt="Caution Scheduling Logo"
+                        className="header-logo"
+                    />
+                </a>
+                <span className="center-text">Caution Scheduling</span>
+                <nav>
+                    <ul className="nav-list">
+                        <li>
+                            <Link to={"/"}>Home</Link>
+                        </li>
+                        <li>
+                            <Link to={"/employee"}>Employee Login</Link>
+                        </li>
+                        <li>
+                            <Link to={"/otherLink"}>Other Link</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+
+            <header className="bottomheader">
+                <nav>
+                    <ul className="nav-list">
+                        <li>
+                            <Link to={"/schedule"}>Lab Schedule</Link>
+                        </li>
+                        <li>
+                            <a href="#meeting">Schedule a Meeting</a>
+                        </li>
+                        <li>
+                            <a href="#intolab">Sign into Lab</a>
+                        </li>
+                        <li>
+                            <a href="#virtual">Join Virtually</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+<Outlet/>
+        </>
+    )
+}
