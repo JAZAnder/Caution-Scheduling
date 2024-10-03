a664cb28f24d1d0753714868d8f6a6de684d5073
Joshua Cantu <joshua@thecantu.com>
2024-10-02 22:08:56 -0500

    Removed Database Folder

--- Caution-Scheduling-API/internal/app/app.go
+++ Caution-Scheduling-API/internal/app/app.go
@@ -2,16 +2,12 @@ package app
 
 import (
 	"database/sql"
-	"fmt"
-	"log"
 	"net/http"
-	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 
 	database "github.com/JAZAnder/Caution-Scheduling/internal/helpers/database"
-
 )
 
 type App struct {
@@ -23,41 +19,14 @@ func (a *App) Initialize() {
 
 	a.DB = database.GetDatabase()
 
-
-	checkDatabase(a.DB)
-
 	//Creates Router
 	a.Router = mux.NewRouter()
 
 	a.initializeRoutes()
 
- }
-
-func (a *App) Run(addr string) { 
-
-	http.ListenAndServe(":" + addr, a.Router)
 }
 
+func (a *App) Run(addr string) {
 
-func checkDatabase(db *sql.DB){
-	dir := "./mysql-tables"
-
-	files, err := os.ReadDir(dir)
-	if err != nil{
-		log.Fatal(err)
-	}
-
-	for _, file := range files {
-		if !file.IsDir(){
-			filePath := fmt.Sprintf("%s/%s", dir, file.Name())
-			content, err := os.ReadFile(filePath)
-			if err != nil {
-				log.Printf("Failed to read file %s: %v", file.Name(), err)
-			} else {
-				_ ,err := db.Exec(string(content))
-				if err != nil {fmt.Println(err)}
-			}
-
-		}
-	}
+	http.ListenAndServe(":"+addr, a.Router)
 }
