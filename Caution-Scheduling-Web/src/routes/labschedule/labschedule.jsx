7318032fb3df15639b0b248dc243190f537514a5
ItsChasesHub <chase.leimbach@selu.edu>
2024-10-08 13:18:09 -0500

    CSS Page Changes

--- Caution-Scheduling-Web/src/routes/labschedule/labschedule.jsx
+++ Caution-Scheduling-Web/src/routes/labschedule/labschedule.jsx
@@ -3,11 +3,11 @@ import "./labschedule.css";
 import EnvocLab from "../../assets/EnvocLab.png";
 import FayLab from "../../assets/Fay125Lab.png";
 import Background from "../../background";
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import useMediaQuery from '../../context/useMediaQuery.jsx';
+import { Container, Row, Col, Button } from "react-bootstrap";
+import useMediaQuery from "../../context/useMediaQuery.jsx";
 
 const LabSchedule = () => {
-  const isMobile = useMediaQuery('(max-width: 900px)');
+  const isMobile = useMediaQuery("(max-width: 900px)");
 
   return (
     <div className="lab-schedule-page">
@@ -16,15 +16,39 @@ const LabSchedule = () => {
         // React Bootstrap layout for mobile
         <Container className="mobile-lab-schedule">
           <div className="mobile-lab-content">
-            <img src={EnvocLab} alt="Envoc Lab" className="img-fluid mobile-lab-image" />
-            <a href="http://meet.google.com/cgc-diaj-gnx" className="d-block mt-2">
-              <Button variant="primary">Envoc Google Link</Button>
+            <img
+              src={EnvocLab}
+              alt="Envoc Lab"
+              className="img-fluid mobile-lab-image"
+            />
+            <a
+              href="http://meet.google.com/cgc-diaj-gnx"
+              className="d-block mt-2"
+            >
+              <Button
+                variant="primary"
+                style={{ backgroundColor: "#1a5632", color: "white" }}
+              >
+                Envoc Google Link
+              </Button>
             </a>
           </div>
           <div className="mobile-lab-content">
-            <img src={FayLab} alt="Fay 125 Lab" className="img-fluid mobile-lab-image" />
-            <a href="http://meet.google.com/fda-vanx-mnd" className="d-block mt-2">
-              <Button variant="primary">Fayard Google Link</Button>
+            <img
+              src={FayLab}
+              alt="Fay 125 Lab"
+              className="img-fluid mobile-lab-image"
+            />
+            <a
+              href="http://meet.google.com/fda-vanx-mnd"
+              className="d-block mt-2"
+            >
+              <Button
+                variant="primary"
+                style={{ backgroundColor: "#1a5632", color: "white" }}
+              >
+                Fayard Google Link
+              </Button>
             </a>
           </div>
         </Container>
@@ -33,17 +57,33 @@ const LabSchedule = () => {
         <>
           <div className="lab-schedule-container1">
             <div className="lab-schedule-content">
-              <img src={EnvocLab} alt="Envoc Lab" className="lab-schedule-image" />
-              <a href="http://meet.google.com/cgc-diaj-gnx" className="labschedulebutton-link">
+              <img
+                src={EnvocLab}
+                alt="Envoc Lab"
+                className="lab-schedule-image"
+              />
+              <a
+                href="http://meet.google.com/cgc-diaj-gnx"
+                className="labschedulebutton-link"
+              >
                 <button className="labschedulebutton">Envoc Google Link</button>
               </a>
             </div>
           </div>
           <div className="lab-schedule-container2">
             <div className="lab-schedule-content">
-              <img src={FayLab} alt="Fay 125 Lab" className="lab-schedule-image" />
-              <a href="http://meet.google.com/fda-vanx-mnd" className="labschedulebutton-link">
-                <button className="labschedulebutton">Fayard Google Link</button>
+              <img
+                src={FayLab}
+                alt="Fay 125 Lab"
+                className="lab-schedule-image"
+              />
+              <a
+                href="http://meet.google.com/fda-vanx-mnd"
+                className="labschedulebutton-link"
+              >
+                <button className="labschedulebutton">
+                  Fayard Google Link
+                </button>
               </a>
             </div>
           </div>
