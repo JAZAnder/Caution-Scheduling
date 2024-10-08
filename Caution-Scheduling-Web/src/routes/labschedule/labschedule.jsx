8cd54a54cfb6eac0250415dfab15a8241894232c
ItsChasesHub <chase.leimbach@selu.edu>
2024-10-08 13:12:41 -0500

    Bootstrapped Pages

--- Caution-Scheduling-Web/src/routes/labschedule/labschedule.jsx
+++ Caution-Scheduling-Web/src/routes/labschedule/labschedule.jsx
@@ -3,27 +3,52 @@ import "./labschedule.css";
 import EnvocLab from "../../assets/EnvocLab.png";
 import FayLab from "../../assets/Fay125Lab.png";
 import Background from "../../background";
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import useMediaQuery from '../../context/useMediaQuery.jsx';
 
 const LabSchedule = () => {
+  const isMobile = useMediaQuery('(max-width: 900px)');
+
   return (
     <div className="lab-schedule-page">
       <Background />
-      <div className="lab-schedule-container1">
-        <div className="lab-schedule-content">
-          <img src={EnvocLab} alt="Envoc Lab" className="lab-schedule-image" />
-          <a href="http://meet.google.com/cgc-diaj-gnx" className="labschedulebutton-link">
-            <button className="labschedulebutton">Envoc Google Link</button>
-          </a>
-        </div>
-      </div>
-      <div className="lab-schedule-container2">
-        <div className="lab-schedule-content">
-          <img src={FayLab} alt="Fay 125 Lab" className="lab-schedule-image" />
-          <a href="http://meet.google.com/fda-vanx-mnd" className="labschedulebutton-link">
-            <button className="labschedulebutton">Fayard Google Link</button>
-          </a>
-        </div>
-      </div>
+      {isMobile ? (
+        // React Bootstrap layout for mobile
+        <Container className="mobile-lab-schedule">
+          <div className="mobile-lab-content">
+            <img src={EnvocLab} alt="Envoc Lab" className="img-fluid mobile-lab-image" />
+            <a href="http://meet.google.com/cgc-diaj-gnx" className="d-block mt-2">
+              <Button variant="primary">Envoc Google Link</Button>
+            </a>
+          </div>
+          <div className="mobile-lab-content">
+            <img src={FayLab} alt="Fay 125 Lab" className="img-fluid mobile-lab-image" />
+            <a href="http://meet.google.com/fda-vanx-mnd" className="d-block mt-2">
+              <Button variant="primary">Fayard Google Link</Button>
+            </a>
+          </div>
+        </Container>
+      ) : (
+        // Custom CSS layout for desktop
+        <>
+          <div className="lab-schedule-container1">
+            <div className="lab-schedule-content">
+              <img src={EnvocLab} alt="Envoc Lab" className="lab-schedule-image" />
+              <a href="http://meet.google.com/cgc-diaj-gnx" className="labschedulebutton-link">
+                <button className="labschedulebutton">Envoc Google Link</button>
+              </a>
+            </div>
+          </div>
+          <div className="lab-schedule-container2">
+            <div className="lab-schedule-content">
+              <img src={FayLab} alt="Fay 125 Lab" className="lab-schedule-image" />
+              <a href="http://meet.google.com/fda-vanx-mnd" className="labschedulebutton-link">
+                <button className="labschedulebutton">Fayard Google Link</button>
+              </a>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
