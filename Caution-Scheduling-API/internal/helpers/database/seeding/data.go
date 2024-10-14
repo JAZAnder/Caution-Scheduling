3877e833d399287263dcc7accfae92fac9f82236
Joshua Cantu <joshua@thecantu.com>
2024-10-14 10:23:17 -0500

    Seeded more users

--- Caution-Scheduling-API/internal/helpers/database/seeding/data.go
+++ Caution-Scheduling-API/internal/helpers/database/seeding/data.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/JAZAnder/Caution-Scheduling/internal/helpers/logger"
 	"github.com/JAZAnder/Caution-Scheduling/internal/objects/user"
-
 )
 
 var database *sql.DB
@@ -17,22 +16,85 @@ func SeedData(db *sql.DB) {
 }
 
 func seedUsers() {
+	//Seeding Administrator User
 	var Admin user.LocalUser = user.LocalUser{
 		UserName:  "Admin",
 		FirstName: "System",
 		LastName:  "Administrator",
-		FullName: "System Administrator",
+		FullName:  "System Administrator",
 		Email:     "admin@localhost.com",
 		Password:  "P@33word123!",
 		IsAdmin:   true,
-		Role: "Administrator",
-
+		Role:      "Administrator",
 	}
 
 	err := Admin.SignUp(database)
 
 	if err != nil {
 		logger.Log(2, "database", "Seeding Data", "System", Admin.UserName+" user is Created")
+	} else {
+		logger.Log(3, "database", "Seeding Data", "System", err.Error())
+	}
+
+	//Seeding Student User
+	err = nil;
+	var student user.LocalUser = user.LocalUser{
+		UserName:  "Student",
+		FirstName: "Normal",
+		LastName:  "Student",
+		FullName:  "Normal Student",
+		Email:     "student@localhost.com",
+		Password:  "P@33word123!",
+		IsAdmin:   false,
+		Role:      "Student",
+	}
+
+	err = student.SignUp(database)
+
+	if err != nil {
+		logger.Log(2, "database", "Seeding Data", "System", student.UserName+" user is Created")
+	} else {
+		logger.Log(3, "database", "Seeding Data", "System", err.Error())
+	}
+
+	//Supervisor  User
+	err = nil;
+	var supervisor user.LocalUser = user.LocalUser{
+		UserName:  "Supervisor",
+		FirstName: "Tutor",
+		LastName:  "Supervisor",
+		FullName:  "Tutor Supervisor",
+		Email:     "supervisor@localhost.com",
+		Password:  "P@33word123!",
+		IsAdmin:   false,
+		Role:      "Supervisor",
+	}
+
+	err = supervisor.SignUp(database)
+
+	if err != nil {
+		logger.Log(2, "database", "Seeding Data", "System", supervisor.UserName+" user is Created")
+	} else {
+		logger.Log(3, "database", "Seeding Data", "System", err.Error())
+	}
+
+	//Supervisor  User
+	err = nil;
+	var Tutor user.LocalUser = user.LocalUser{
+		UserName:  "Tutor",
+		FirstName: "School",
+		LastName:  "Tutor",
+		FullName:  "School Tutor",
+		Email:     "tutor@localhost.com",
+		Password:  "P@33word123!",
+		IsAdmin:   false,
+		Role:      "Tutor",
+	}
+
+	err = Tutor.SignUp(database)
+
+	if err != nil {
+		logger.Log(2, "database", "Seeding Data", "System", Tutor.UserName+" user is Created")
 	} else {
 		logger.Log(3, "database", "Seeding Data", "System", err.Error())
 	}
