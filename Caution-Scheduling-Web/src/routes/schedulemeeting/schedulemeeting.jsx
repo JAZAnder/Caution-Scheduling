fcb324ffd763602911df79d544fe09b2ce03117e
Joshua Cantu <joshua@thecantu.com>
2024-10-24 16:18:13 -0500

    Caps issue

--- Caution-Scheduling-Web/src/routes/schedulemeeting/schedulemeeting.jsx
+++ Caution-Scheduling-Web/src/routes/schedulemeeting/schedulemeeting.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import Background from "../../background";
-import "./ScheduleMeeting.css";
+import "./scheduleMeeting.css";
 import axios from "axios";
 
 const ScheduleMeeting = ({ isAdmin }) => {
