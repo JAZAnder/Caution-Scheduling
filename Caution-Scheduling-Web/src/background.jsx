d3cf0bb0f1ab48b51c22f98dcd4d7554ae5de646
ItsChasesHub <chase.leimbach@selu.edu>
2024-10-29 11:58:25 -0500

    CSS Background Replacment

--- Caution-Scheduling-Web/src/background.jsx
+++ Caution-Scheduling-Web/src/background.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
-import BackgroundImage from "./assets/treesbackground.webp";
+import BackgroundImage from "./assets/backgroundimage.webp";
 
 export default function Background() {
   return (
