e1e82fbd3c5747db67a66a452d38cc85584b168e
Joshua Cantu <joshua@thecantu.com>
2024-10-30 10:22:12 -0500

    Added active to hours

--- Caution-Scheduling-API/internal/helpers/database/seeding/create-tables.go
+++ Caution-Scheduling-API/internal/helpers/database/seeding/create-tables.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 
 	"github.com/JAZAnder/Caution-Scheduling/internal/helpers/logger"
-
 )
 
 func CreateTables(db *sql.DB) {
@@ -19,6 +18,7 @@ func CreateTables(db *sql.DB) {
 	createUserHoursTable()
 	createSessionCookieTable()
 	createLogsTable()
+	createGlobalSettingsTable()
 }
 
 func createLocalUserTables() {
@@ -66,12 +66,32 @@ func createUserSettingsTable() {
 	}
 }
 
+func createGlobalSettingsTable() {
+	query := "CREATE TABLE IF NOT EXISTS userSettings(" +
+		"`Id` int AUTO_INCREMENT PRIMARY KEY," +
+		"`userName` varchar(255) unique," +
+		"`ReceiveMeetingEmails` boolean NOT NULL" +
+		");"
+
+	logger.Log(2, "database", "Create Table", "System", "Creating localUsers table")
+
+	logger.Log(1, "database", "Create Table", "System", query)
+
+	_, err := database.Exec(query)
+	if err != nil {
+		logger.Log(4, "database", "Create Table", "System", err.Error())
+	} else {
+		logger.Log(2, "database", "Create Table", "System", "userSettings table either created or already existed")
+	}
+}
+
 func createHoursTable() {
 	query := "CREATE TABLE IF NOT EXISTS hours(" +
 		"`Id` int AUTO_INCREMENT PRIMARY KEY," +
 		"`startTime` varchar(255) NOT NULL," +
 		"`endTime` varchar(225) NOT NULL," +
-		"`dayOfWeek` int DEFAULT NULL);"
+		"`dayOfWeek` int DEFAULT NULL," +
+		"`active` boolean DEFAULT 0);"
 
 	logger.Log(2, "database", "Create Table", "System", "Creating hours table")
 
