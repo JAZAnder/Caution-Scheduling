16de9167c75a740a335ad0a2334aff55d8d6249e
Dmytro Levytskyi <Dmytro.levytskyi@selu.edu>
2024-11-13 22:19:12 -0600

    Google login credentials
    Added:
    user role now 1 by default,
    Full name

--- Caution-Scheduling-API/internal/controllers/users/everyone.go
+++ Caution-Scheduling-API/internal/controllers/users/everyone.go
@@ -40,6 +40,8 @@ func googleLoginUser(w http.ResponseWriter, r *http.Request) {
 		u.FirstName = googleUser.GivenName
 		u.LastName = googleUser.FamilyName
 		u.UserName = strings.ToLower(googleUser.GivenName + "." + googleUser.FamilyName)
+		u.Role = 1
+		u.FullName = u.FirstName + " " + u.LastName
 		u.GoogleId = googleUser.Sub
 		if err = u.SignUp(database); err != nil {
 			responses.RespondWithError(w, http.StatusInternalServerError, "Failed to create user")
