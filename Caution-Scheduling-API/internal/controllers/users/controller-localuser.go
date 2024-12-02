b6c452b38f5bc23386972683b7dee13322b80863
Joshua Cantu <joshua@thecantu.com>
2024-12-02 09:59:46 -0600

    Moved fuction

--- Caution-Scheduling-API/internal/controllers/users/controller-localuser.go
+++ Caution-Scheduling-API/internal/controllers/users/controller-localuser.go
@@ -130,61 +130,7 @@ func isAdmin(r *http.Request) (bool, error) {
 }
 
 
-func resetPassword(w http.ResponseWriter, r *http.Request) {
-	var c user.SessionCookie
-	var u user.LocalUser
-	cookie, err := r.Cookie("key")
-	if err != nil {
-		if errors.Is(err, http.ErrNoCookie) {
-			responses.RespondWithError(w, http.StatusUnauthorized, "Cookie not Found")
-			return
-		} else {
-			responses.RespondWithError(w, http.StatusInternalServerError, err.Error())
-			return
-		}
-	}
 
-	c.Cookie = cookie.Value
-
-	currentUser, err := c.CheckSession(database)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			responses.RespondWithError(w, http.StatusUnauthorized, "Session Expired")
-			return
-		} else {
-			responses.RespondWithError(w, http.StatusInternalServerError, err.Error())
-			return
-		}
-	}
-
-	
-	authorized, err := currentUser.HasSupervisorRights()
-
-	if err != nil {
-		responses.RespondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	if !authorized {
-		responses.RespondWithError(w, http.StatusForbidden, "Insufficient Permissions")
-		return
-	}
-
-
-	u.UserName = r.PostFormValue("UserName")
-
-
-	u.GetUser(database)
-
-	if u.Role > currentUser.Role  {
-		responses.RespondWithError(w, http.StatusForbidden, "Supervisors are Unable to reset Administrator Passwords")
-		return
-	}
-
-	u.Password = r.PostFormValue("password")
-	u.ChangePassword(database)
-	responses.RespondWithJSON(w, http.StatusCreated, "Password Changed")
-}
 // func addTime(w http.ResponseWriter, r *http.Request) {
 // 	var c user.SessionCookie
 // 	var uh userHour.UserHour
