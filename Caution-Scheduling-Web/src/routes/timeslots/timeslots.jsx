4eb1835924951e438764d00018a511a06fc1f074
Joshua Cantu <joshua@thecantu.com>
2024-12-02 20:30:18 -0600

    Redid table

--- Caution-Scheduling-Web/src/routes/timeslots/timeslots.jsx
+++ Caution-Scheduling-Web/src/routes/timeslots/timeslots.jsx
@@ -59,37 +59,37 @@ const timeslots = () => {
                 </Form.Group>
                 <Form.Group className="mb-2">
                   <Form.Control
-                    id='id' 
-                    type="text" 
-                    placeholder="Id" 
+                    id='id'
+                    type="text"
+                    placeholder="Id"
                     className="filter-input"
                   />
                 </Form.Group>
                 <Form.Group className="mb-2">
-                  <Form.Control 
+                  <Form.Control
                     id='startTime'
                     value={startTime}
                     onChange={(e) => setStartTime(e.target.value)}
-                    type="text" 
-                    placeholder="Start Time" 
-                    className="filter-input" 
+                    type="text"
+                    placeholder="Start Time"
+                    className="filter-input"
                   />
                 </Form.Group>
                 <Form.Group className="mb-2">
-                  <Form.Control 
+                  <Form.Control
                     id='endTime'
                     value={endTime}
                     onChange={(e) => setEndTime(e.target.value)}
-                    type="text" 
-                    placeholder="End Time" 
-                    className="filter-input"  
+                    type="text"
+                    placeholder="End Time"
+                    className="filter-input"
                   />
                 </Form.Group>
                 <Form.Group className="mb-2">
-                  <Form.Control 
-                    type="text" 
-                    placeholder="DOW" 
-                    className="filter-input" 
+                  <Form.Control
+                    type="text"
+                    placeholder="DOW"
+                    className="filter-input"
                   />
                 </Form.Group>
                 <Button variant="light" className="search-button">Search</Button>
@@ -101,7 +101,7 @@ const timeslots = () => {
             <Col xs={12}>
               <Table striped bordered hover responsive className="timeslots-table">
                 <thead>
-                <tr className="timeslots-tablerow">
+                  <tr className="timeslots-tablerow">
                     <th>Id</th>
                     <th>Day of Week</th>
                     <th>Start Time</th>
@@ -126,38 +126,38 @@ const timeslots = () => {
         <div className="timeslots-outer-container">
           <div className="timeslots-container">
             <div className="timeslots-header">
-              <NewTimeslotButton/>
+              {/* <NewTimeslotButton /> */}
             </div>
 
             <div className="timeslots-filter">
               <label className="timeslots-filter-label">Filter on:</label>
-              <input 
-                type="text" 
-                placeholder="Id" 
-                className="timeslots-filter-input" 
+              <input
+                type="text"
+                placeholder="Id"
+                className="timeslots-filter-input"
               />
-              <input 
+              <input
                 id='startTime'
                 value={startTime}
                 onChange={(e) => setStartTime(e.target.value)}
-                type="text" 
-                placeholder="Start Time" 
-                className="timeslots-filter-input" 
+                type="text"
+                placeholder="Start Time"
+                className="timeslots-filter-input"
               />
-              <input 
+              <input
                 id='endTime'
                 value={endTime}
                 onChange={(e) => setEndTime(e.target.value)}
-                type="text" 
-                placeholder="End Time" 
-                className="timeslots-filter-input"  
+                type="text"
+                placeholder="End Time"
+                className="timeslots-filter-input"
               />
-              <select 
-                className="timeslots-filter-input" 
-                name="dayOfWeek" 
+              <select
+                className="timeslots-filter-input"
+                name="dayOfWeek"
                 id="dayOfWeek"
                 onChange={(e) => setDayOfWeek(e.target.value)}
-              > 
+              >
                 <option value=""> Day of Week </option>
                 <option value="1"> Monday</option>
                 <option value="2"> Tuesday</option>
@@ -166,17 +166,17 @@ const timeslots = () => {
                 <option value="5"> Friday</option>
                 <option value="6"> Saturday</option>
                 <option value="0"> Sunday</option>
-                
+
               </select>
-              
+
 
               <button
-                type="button" 
-                disabled={loading} 
+                type="button"
+                disabled={loading}
                 onClick={resetSearch}
                 className="timeslots-search-button"
               >
-                  {loading ? 'Waiting' : 'Reset Search'}
+                {loading ? 'Waiting' : 'Reset Search'}
               </button>
             </div>
 
@@ -188,7 +188,7 @@ const timeslots = () => {
             />
 
 
-            
+
           </div>
         </div>
       )}
@@ -197,7 +197,7 @@ const timeslots = () => {
 };
 
 
-function ListFilteredTimeSlots({FLdayOfWeek, FLstartTime, FLendTime, debounce}){
+function ListFilteredTimeSlots({ FLdayOfWeek, FLstartTime, FLendTime, debounce }) {
   const { data: timeSlots, loading, error } = useFetch(
     `/api/timeslots`,
     { method: "get" },
@@ -211,35 +211,95 @@ function ListFilteredTimeSlots({FLdayOfWeek, FLstartTime, FLendTime, debounce}){
       </center>
     );
   }
-  return(
+  return (
     <>
-    <table className="timeslots-table">
-              <thead>
-              <tr className="timeslots-tablerow">
-                  <th>Id</th>
-                  <th>Day of Week</th>
-                  <th>Start Time</th>
-                  <th>End Time</th>
-                  <th>Details</th>
-                </tr>
-              </thead>
-              <tbody>
-                {timeSlots &&
-                  Object.keys(timeSlots).map((timeslot, i) =>(
-                    <tr key={i}>
-                      <td>{timeSlots[timeslot].id}</td>
-                      <td>{timeSlots[timeslot].dayOfWeek}</td>
-                      <td>{timeSlots[timeslot].startTime}</td>
-                      <td>{timeSlots[timeslot].endTime}</td>
-                      <td><TimeslotDetailsButton timeslot={timeSlots[timeslot]}/></td>
-                    </tr>
-                  ))
+      <table className="timeslots-table">
+        <thead>
+          <tr className="timeslots-tablerow">
+            <th>Id</th>
+            <th>Day of Week</th>
+            <th>Start Time</th>
+            <th>End Time</th>
+            <th>Scheduled Tutors</th>
+            <th>Active / Disabled</th>
+          </tr>
+        </thead>
+        <tbody>
+          {timeSlots &&
+            Object.keys(timeSlots).map((timeslot, i) => (
+              <tr key={i}>
+                <td>{timeSlots[timeslot].id}</td>
+                <td>{timeSlots[timeslot].dayOfWeek}</td>
+                <td>{timeSlots[timeslot].startTime}</td>
+                <td>{timeSlots[timeslot].endTime}</td>
+                <td>
+                  <Button variant="primary" >
+                    Tutors
+                  </Button>
+                </td>
+                <td>
+                  <TimeslotStateButton  timeslot={timeSlots[timeslot]}/>
+                </td>
+              </tr>
+            ))
 
-                }
-              </tbody>
-            </table>
+          }
+        </tbody>
+      </table>
     </>
   )
 }
+function TimeslotStateButton(timeslot) {
+  const [getState, setState] = useState(timeslot.timeslot.active)
+  console.log(timeslot.timeslot.active)
 
+  const toggleState = async () => {
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append('id', timeslot.timeslot.id);
+    urlencoded.append('active', !getState);
+
+    const requestOptions = {
+      method: 'PUT',
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: 'follow',
+    };
+
+    try {
+      const response = await fetch('/api/hour', requestOptions);
+  
+      if (!response.ok) {
+        throw new Error('Something Went Wrong');
+      }
+  
+      const data = await response.json();
+      //return data;
+
+    } catch (error) {
+        alert(error)
+      console.error('Error:', error);
+      throw new Error('Error');
+    }
+  }
+
+
+  return (
+    <>
+      {getState ? (
+        <Button variant="secondary" onClick={function () {toggleState(); setState(!getState);  } }>
+          Disable Timeslot
+        </Button>
+      ) : (
+        <Button variant="success" onClick={function () {toggleState(); setState(!getState);  } }>
+          Enable Timeslot
+        </Button>
+      )}
+
+    </>
+
+  )
+}
 export default timeslots;
